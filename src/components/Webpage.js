9d96ac91474866c7f3e61121fce050dbdc62d125
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-25 03:02:05 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -5,8 +5,8 @@ class Webpage extends React.component {
   render(){
     return (
       <h1>The world's coolest webpage</h1>
-      <FillerText />
-      <FillerText />
+      <FillerText></FillerText>
+      <FillerText></FillerText>
     )
   }
 }
