00ff702dcc79a193981c0a36370206d06dfbacdf
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-25 03:02:23 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -4,7 +4,7 @@ import FillerText from './FillerText'
 class Webpage extends React.component {
   render(){
     return (
-      <h1>The world's coolest webpage</h1>
+      <h1>The worlds coolest webpage</h1>
       <FillerText></FillerText>
       <FillerText></FillerText>
     )
