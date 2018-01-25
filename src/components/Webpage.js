f181e92219020cb6224ce743dd875e1fc8fb1bf5
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-25 02:51:58 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -2,8 +2,7 @@ import React from 'react'
 import FillerText from './fillertext'
 class Webpage extends React.component {
   render(){
-    return ( <form>
-            </form>
+    return ( 
           )
   }
 }
