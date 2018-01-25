e46b58567fc494ddcd0966f54d7a9e79951b147f
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-25 02:53:37 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 class FillerText extends React.component {
   render(){
-    return ( 
-          )
+    return (
+      
+    )
   }
 }
 
--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import FillerText from './fillertext'
 class Webpage extends React.component {
   render(){
-    return ( 
+    return (
               hello world
     )
   }
