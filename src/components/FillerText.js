5baf22b84dbf9cb72b5fc89fece1517199bf6b34
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-25 02:55:59 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -3,7 +3,7 @@ import React from 'react'
 class FillerText extends React.component {
   render(){
     return (
-      hello world
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
     )
   }
 }
