648b8f1c58051fadf52a5e828a7a399d61933bca
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-25 02:50:59 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 class RegistrationForm extends React.component {
-  render()
+  render(){
     return ( <form>
             </form>
           )
+  }
 }
 
 export default RegistrationForm
