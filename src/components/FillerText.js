1be06c81e69b64f0007d13e1022976157ebb99e0
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-25 02:50:50 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 class FillerText extends React.component {
-  render()
+  render(){
     return ( <form>
             </form>
           )
+  }
 }
 
 export default FillerText
